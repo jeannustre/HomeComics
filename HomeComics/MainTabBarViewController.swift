72f286a1361a2296805597929d23fc512285d978
jeannustre <jean.sarda@epitech.eu>
2017-05-03 23:22:07 +0200

    changed tab bar items spacing to fill window width

---
+++ HomeComics/MainTabBarViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MainTabBarViewController.swift
+//  HomeComics
+//
+//  Created by Jean Sarda on 03/05/2017.
+//  Copyright © 2017 Jean Sarda. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.tabBar.itemPositioning = .fill
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
