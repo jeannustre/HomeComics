b1ee0c38d32a345fe9cf84e63df15f5526450b6a
jeannustre <jean.sarda@epitech.eu>
2017-05-11 03:58:50 +0200

    dummy popover

---
+++ HomeComics/InfoPopOverViewController.swift
@@ -0,0 +1,37 @@
+//
+//  InfoPopOverViewController.swift
+//  HomeComics
+//
+//  Created by Jean Sarda on 11/05/2017.
+//  Copyright © 2017 Jean Sarda. All rights reserved.
+//
+
+import UIKit
+
+class InfoPopOverViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        print("yas")
+//        self.popoverPresentationController.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
