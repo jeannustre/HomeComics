b1c0faf920c490cb9e2c1bd61770a31f66e57809
jeannustre <jean.sarda@epitech.eu>
2017-05-12 16:34:21 +0200

    Collection view layout extension - hardcoded 3 books per row

--- HomeComics/BooksViewController.swift
+++ HomeComics/BooksViewController.swift
@@ -151,4 +151,26 @@ extension BooksViewController: UISearchBarDelegate {
     
 }
 
+//MARK: - Layout Delegate
+fileprivate let sectionInsets = UIEdgeInsets(top: 10.0, left: 10.0, bottom: 10.0, right: 10.0)
+fileprivate let itemsPerRow = 3
+extension BooksViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let paddingSpace = sectionInsets.left * CGFloat((itemsPerRow + 1))
+        let availableWidth = view.frame.width - paddingSpace
+        let widthPerItem = availableWidth / CGFloat(itemsPerRow)
+        return CGSize(width: widthPerItem, height: widthPerItem * 1.5)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return sectionInsets
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return sectionInsets.left
+    }
+    
+}
+
 
